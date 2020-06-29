9ebea83ae754c87cf02a6c6d6daa9034f0dca07f
zhangwei <245370344@qq.com>
2020-06-29 11:07:58 +0800

    first commit

---
+++ springcloud-elasticsearch5_6_8-zhangwei/src/main/java/com/zhangwei/controller/ArticleController.java
@@ -0,0 +1,46 @@
+package com.zhangwei.controller;
+
+import com.zhangwei.bean.Article;
+import com.zhangwei.repository.ArticleRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+
+/**
+ * @author zhangwei
+ * @date 2020-06-28
+ * <p>
+ */
+@RestController
+@RequestMapping(value = "/es")
+@Api(value = "elasticsearch", description = "elasticsearch管理")
+public class ArticleController {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    private ElasticsearchTemplate elasticsearchTemplate;
+
+    @GetMapping("/createIndex")
+    @PostMapping(value = "/create")
+    @ApiOperation(value = "创建索引及类型", tags = "elasticsearch管理")
+    public String createIndex() {
+        return elasticsearchTemplate.createIndex(Article.class) ? "创建索引及类型成功" : "创建索引及类型失败";
+    }
+
+    @PostMapping(value = "/addDocumnet")
+    @ApiOperation(value = "创建文档", tags = "elasticsearch管理")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "article", value = "文档内容", required = true,  dataType = "Article", paramType = "body")
+    })
+    public Article addDocumnet(@RequestBody Article article) {
+        return articleRepository.save(article);
+    }
+}
---
+++ springcloud-elasticsearch5_6_8-zhangwei/src/main/java/com/zhangwei/repository/ArticleRepository.java
@@ -0,0 +1,14 @@
+package com.zhangwei.repository;
+
+import com.zhangwei.bean.Article;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author zhangwei
+ * @date 2020-06-28
+ * <p>
+ */
+@Repository
+public interface ArticleRepository extends ElasticsearchRepository<Article, Long> {
+}
