3894b210d80cb36ebbae38fa9d5d8466a85e34ca
zhangwei <245370344@qq.com>
2020-08-16 13:57:34 +0800

    first commit

---
+++ springcloud-cos-zhangwei/src/main/java/com/zhangwei/SpringcloudCosZhangweiApplication.java
@@ -0,0 +1,13 @@
+package com.zhangwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudCosZhangweiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudCosZhangweiApplication.class, args);
+    }
+
+}
