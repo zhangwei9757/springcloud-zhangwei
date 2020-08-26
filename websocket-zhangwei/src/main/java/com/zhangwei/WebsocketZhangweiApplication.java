ce0bab57adfcf323d2a15097f840234a362b20d2
zhangwei <245370344@qq.com>
2020-08-26 23:03:04 +0800

    first commit

--- websocket-zhangwei/src/main/java/com/zhangwei/WebsocketZhangweiApplication.java
+++ websocket-zhangwei/src/main/java/com/zhangwei/WebsocketZhangweiApplication.java
@@ -1,9 +1,11 @@
 package com.zhangwei;
 
+import com.zhangwei.annotation.EnableWebSocketEndpoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableWebSocketEndpoint
 public class WebsocketZhangweiApplication {
 
     public static void main(String[] args) {
--- websocket-zhangwei/src/main/java/com/zhangwei/utils/ByteUtils.java
+++ websocket-zhangwei/src/main/java/com/zhangwei/utils/ByteUtils.java
@@ -1,43 +1,43 @@
-package com.zhangwei.utils;
-
-/**
- * @author zhangwei
- * @date 2020-08-22
- * <p> 字节数组工具类
- */
-public class ByteUtils {
-
-    /**
-     * 1 个字节 转 int
-     *
-     * @param b
-     * @return
-     */
-    public static int byte2int(byte[] b) {
-        int res = 0;
-        int bLen = b.length;
-
-        if (bLen < 5) {
-            for (int i = 0; i < bLen; i++) {
-                res += (b[i] & 0xFF) << (8 * i);
-            }
-        }
-        return res;
-    }
-
-    /**
-     * 4个字节数组 转 int
-     *
-     * @param b      字节数组
-     * @param offset 位置
-     * @return
-     */
-    public static int byteArrayToInt(byte[] b, int offset) {
-        int value = 0;
-        for (int i = 0; i < 4; i++) {
-            int shift = (4 - 1 - i) * 8;
-            value += (b[i + offset] & 0x000000FF) << shift;
-        }
-        return value;
-    }
-}
+//package com.zhangwei.utils;
+//
+///**
+// * @author zhangwei
+// * @date 2020-08-22
+// * <p> 字节数组工具类
+// */
+//public class ByteUtils {
+//
+//    /**
+//     * 1 个字节 转 int
+//     *
+//     * @param b
+//     * @return
+//     */
+//    public static int byte2int(byte[] b) {
+//        int res = 0;
+//        int bLen = b.length;
+//
+//        if (bLen < 5) {
+//            for (int i = 0; i < bLen; i++) {
+//                res += (b[i] & 0xFF) << (8 * i);
+//            }
+//        }
+//        return res;
+//    }
+//
+//    /**
+//     * 4个字节数组 转 int
+//     *
+//     * @param b      字节数组
+//     * @param offset 位置
+//     * @return
+//     */
+//    public static int byteArrayToInt(byte[] b, int offset) {
+//        int value = 0;
+//        for (int i = 0; i < 4; i++) {
+//            int shift = (4 - 1 - i) * 8;
+//            value += (b[i + offset] & 0x000000FF) << shift;
+//        }
+//        return value;
+//    }
+//}
