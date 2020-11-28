ba8d5f65033d36830062b6be8dccab04e47f1349
zhangwei <245370344@qq.com>
2020-11-28 15:22:46 +0800

    sentinel源码Tag_V1.8.0标签

--- sentinel-zhangwei/src/main/java/com/microservice/SentinelZhangweiApplication.java
+++ sentinel-zhangwei/src/main/java/com/microservice/SentinelZhangweiApplication.java
@@ -20,6 +20,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 参数	作用
  * -Dcsp.sentinel.dashboard.server=localhost:8080	向 Sentinel 接入端指定控制台的地址
  * -Dproject.name=sentinel-dashboard	向 Sentinel 指定应用名称，比如上面对应的应用名称就为 sentinel-dashboard
+ * <p>
+ * step3: 开启限流配置
+ * https://github.com/alibaba/Sentinel/wiki/%E4%B8%BB%E6%B5%81%E6%A1%86%E6%9E%B6%E7%9A%84%E9%80%82%E9%85%8D
  */
 @SpringBootApplication
 public class SentinelZhangweiApplication {
@@ -28,4 +31,8 @@ public class SentinelZhangweiApplication {
         SpringApplication.run(SentinelZhangweiApplication.class, args);
     }
 
+    /**
+     * Spring Cloud Sentinel 限流接入文档
+     * https://github.com/alibaba/spring-cloud-alibaba/wiki/Sentinel#%E5%A6%82%E4%BD%95%E4%BD%BF%E7%94%A8-sentinel
+     */
 }
