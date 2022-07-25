3422a6efc373dada5853bb4670e59c4525445525
13545629757 <zhangwei19890214>
2022-07-25 10:12:45 +0800

    update101

--- webflux-zhangwei/src/main/java/com/microservice/controller/IndexController.java
+++ webflux-zhangwei/src/main/java/com/microservice/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.microservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 /**
  * @author zw
@@ -10,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
-//    @GetMapping(value = "/index")
-//    public Mono<String> index() {
-//        return Mono.justOrEmpty("index.....");
-//    }
+    @GetMapping(value = "/index")
+    public Mono<String> index() {
+        return Mono.justOrEmpty("index.....");
+    }
 }
